bf69dcb374e227fc3c147539e1d5ad1f7428ad39
Siddhartha Somadder <ssomadder40@stuy.edu>
2023-01-09 23:07:03 -0500

    test for nodupe method

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
 public class Driver{
   public static void main(String[] args){
-    Recursion.printAllWords(6);
+    //Recursion.printAllWords(6);
+    char[] letters = {'a', 'b', 'c', 'd', 'e'};
+    Recursion.printNoDoubleLetterWords(5, letters);
   }
 }
